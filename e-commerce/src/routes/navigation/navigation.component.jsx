40b4f646b29ba1eed2fe844f13e9d35d8f392592
kushal <kushalbhetuwal@gmail.com>
2023-08-15 13:08:10 +1000

    added reactrouter

---
+++ e-commerce/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Outlet, Link } from "react-router-dom"
+import { Fragment } from "react"
+import './navigation.component.scss'
+
+const Navigation = ()=>{
+    return (
+      <Fragment>
+   <div className="navigation">
+       <div>
+       <Link  className="logo-container" to ='/'>
+       <i class="fa-brands fa-cc-apple-pay" style={{color: "#333333", fontSize:'80px'}}></i>
+       </Link>
+       </div>
+         <div className='nav-links-container'>
+            <Link className="nav-link" to='/shop'>
+              Shop
+            </Link>
+         </div>
+      </div>
+    <Outlet/>
+      </Fragment>
+     
+    )
+  }
+
+  export default Navigation
