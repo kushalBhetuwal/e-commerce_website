b2af80f70b1e28b2818f9ffa84b23cd65ec6ea02
kushal <kushalbhetuwal@gmail.com>
2023-08-21 02:41:06 +1000

    styled the authentication page

--- e-commerce/src/routes/authentication/authentication.jsx
+++ e-commerce/src/routes/authentication/authentication.jsx
@@ -1,27 +1,13 @@
-import { createuserdocumentfromauth, auth } from "../../utlils/firebase/firebase.js";
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
 import SignupForm from "../../components/signupform/signupform.component.jsx";
 import SigninForm from  "../../components/signinform/signinform.component.jsx";
+import './authentication.scss';
 
 
 const Authentication =()=>{
-useEffect(()=>{
-  const fetchdata = async ()=>{
-    const response = await getRedirectResult(auth); 
-    if(response){
-      createuserdocumentfromauth(response.user)
-    }
-  }
-  fetchdata();
-}, [])
     return(
-        <div>
-        <h1>Sign in Page</h1>
-       <SigninForm/>
-          <h2>Sign up Page</h2>
-          <SignupForm/>
-
+        <div className = "authentication-container">
+        <SigninForm/>
+        <SignupForm/>
         </div>
     )
 }
